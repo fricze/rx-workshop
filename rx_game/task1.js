e990af928d0a09adc6a03c96e4886592b74344bf
Andrzej <andrzej.fricze@codete.co>
2016-10-01 14:36:13 +0200

    Solve task 1

--- rx_game/task1.js
+++ rx_game/task1.js
@@ -5,7 +5,6 @@ import { keyCodes } from './keycodes';
 const keyDown$ = Rx.Observable.fromEvent(window, 'keydown');
 const charsToType = 'lettergame'.split('');
 
-
 // 1) Combine Rx operators in order to make a type game functionality.
 // Every keydown event should be put on a stream, mapped into key code to a char
 // and then compared with chars from given array (charsToType)
@@ -13,21 +12,31 @@ const charsToType = 'lettergame'.split('');
 // char should be removed (so now, the second char will go onto first place)
 // Stay pure and do not change charsToType array - only its copy
 
+const identity = x => x;
 
 // in order to get a proper property from a stream we can use `pluck` operator
-export const lettersSubscription = keyDown$.pluck("keyCode")
-    // to do something that will not effect a value on a stream we use `do` operator
-    .do(keyCode => console.log(keyCode))
-    // 1. map keyCodes to the letters
-    .map(keyCode => keyCode)
-    // 2. use `scan` to apply logic of slicing array of characters
-    .scan((acc, pressedKey) => {
-        return charsToType;
-    }, charsToType)
-    // 3. filter on empty array of characters
-    .filter(charArr => charrArr)
-    // 4. subscribe and log the winner message
-    .subscribe(emptyCharrArr => {
-        console.log('i am subscribed')
-        // 5. dispose subscription
-    });
+export const letters$ = keyDown$.pluck("keyCode")
+// to do something that will not effect a value on a stream we use `do` operator
+  .do(keyCode => console.log(keyCode))
+// 1. map keyCodes to the letters
+  .map(keyCode => keyCodes[keyCode])
+  .filter(identity)
+// 2. use `scan` to apply logic of slicing array of characters
+  .scan((acc, pressedKey) => acc.slice(pressedKey === acc[0]),
+        charsToType) // initial value
+  .do(string => console.log(string))
+
+letters$.filter(charArr => !!charArr.length)
+// 4. subscribe and log the winner message
+  .subscribe(() => {
+    console.log('you are still playing!')
+    // 5. dispose subscription
+  });
+
+// 3. filter on empty array of characters
+letters$.filter(charArr => !charArr.length)
+// 4. subscribe and log the winner message
+  .subscribe(() => {
+    console.log('you won!')
+    // 5. dispose subscription
+  });
